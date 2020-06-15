1797c3f3b71d0ea50b0cab045fa8d6973cc0098a
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-15 09:29:27 -0700

    comments, params
    playing with the cube code

--- cube.groovy
+++ cube.groovy
@@ -1,1 +1,6 @@
-//Your code here
+
+//this shape cannot be smaller than the default that it was forked from?!
+return new Cube(	200,// X dimensional radius, in 1 mm increments
+			200,// Y dimension radius, in 1 mm increments
+			200//  Z dimension radius, in 1 mm increments
+			).toCSG()// this converts from the geometry to an object we can work with
