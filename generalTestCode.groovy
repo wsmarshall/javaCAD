2fe9a812e6a2ff5145a017dc1eca07ebc1ebfee4
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-19 13:23:24 -0700

    factories with params
    progress through tutorial

--- generalTestCode.groovy
+++ generalTestCode.groovy
@@ -6,6 +6,37 @@
 
 
 /**
+ArrayList<Object> servoMeasurments = new ArrayList<Object>();
+
+servoMeasurments.add(11.7) //servoThinDimentionThickness
+servoMeasurments.add(23.6) // servoThickDimentionThickness
+servoMeasurments.add(25.16) // servoShaftSideHeight
+servoMeasurments.add(3.8) // outputShaftDiameter
+servoMeasurments.add(5.13) //shaftToShortSideDistance
+servoMeasurments.add(9.8) // shaftToShortSideFlandgeEdge
+servoMeasurments.add(12.9) // tipOfShaftToBottomOfFlange
+servoMeasurments.add(3.0 ) //flangeThickness
+servoMeasurments.add(32.3) // flangeLongDimention
+servoMeasurments.add(10.16) //bottomOfFlangeToTopOfBody
+
+CSG servoReference =  (CSG)ScriptingEngine
+	                    .gitScriptRun(
+                                "https://gist.github.com/3f9fef17b23acfadf3f7.git", // git location of the library
+	                              "servo.groovy" , // file to load
+	                              servoMeasurments
+                        )
+// Create copy for additional links                      
+CSG copyOfServo =   servoReference
+			.clone()  
+			.movex(40)
+copyOfServo.setColor(javafx.scene.paint.Color.RED);
+
+return [servoReference,
+	copyOfServo
+	]
+*/
+
+/**
 CSG referencedThingy =  (CSG)ScriptingEngine
 	                    .gitScriptRun(
                                 "https://gist.github.com/d0a4fcda488a8958095b.git", // git location of the library
