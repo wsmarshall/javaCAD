4b5fa799f7c381f6d3fc1b398c044a9e0daab734
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-22 10:40:26 -0700

    through extrude text
    tutorial progress

--- generalTestCode.groovy
+++ generalTestCode.groovy
@@ -3,6 +3,44 @@
  * radiuses along the x,y,z axes
  */
 
+import eu.mihosoft.vrl.v3d.*;
+import javafx.scene.text.Font;
+
+Font font = new Font("Arial",  30);
+
+return TextExtrude.text((double)10.0,"Hello World!",font).collect{
+	it.rotx(180)
+	.toZMin()
+}
+
+/**
+double wheelRadius = 33.0
+wheelHeight=17.6
+double radius = 6.0
+CSG profile = new RoundedCube(radius,// x dimention
+					wheelRadius,// y dimention
+				
+				wheelHeight//  Z dimention
+				)
+				.cornerRadius(radius)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+				.toYMin()
+				.toZMin()
+				
+//create a Cylinder
+height = 22.5
+CSG wheel_body = CSG.unionAll(
+		Extrude.revolve(profile,
+		(double)10, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+		(double)180,// degrees through wich it should sweep
+		(int)10)//number of sweep increments
+		)
+		.movez(wheelHeight*1.5)
+
+return [wheel_body,profile ]
+*/
+
+/**
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Extrude
@@ -31,6 +69,7 @@ CSG crossExtrusion2 = Extrude.polygons(innerCross, secondCross)
 
 //return the base shape, the slice, the z extrusion, and the complex extrusion
 return [base.movez(20),polys,crossExtrusion,crossExtrusion2]
+*/
 
 /**
 import eu.mihosoft.vrl.v3d.svg.*;
