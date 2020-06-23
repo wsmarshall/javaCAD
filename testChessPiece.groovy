8d63802491f393b38698a8e7d973a8f8f371e8ac
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-23 11:25:53 -0700

    chess piece

--- testChessPiece.groovy
+++ testChessPiece.groovy
@@ -1,1 +1,7 @@
-//Your code here
+//goal: black queen
+
+CSG mainCylinder  = new Cylinder(7.5, 5, 70, (100)).toCSG()
+
+CSG baseCylinder = new Cylinder(15, 6, 10, (100)).toCSG()
+
+CSG fullPiece = CSG.unionAll([mainCylinder, baseCylinder])
