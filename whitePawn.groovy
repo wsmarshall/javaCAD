3418c944cfefdbaa983fe7090ed005365e814205
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-23 11:28:19 -0700

    main body
    main and base cylinder made

--- whitePawn.groovy
+++ whitePawn.groovy
@@ -1,1 +1,10 @@
-//Your code here
+//goal: whitePawn
+
+CSG mainCylinder  = new Cylinder(7.5, 5, 40, (100)).toCSG()
+
+CSG baseCylinder = new Cylinder(15, 6, 10, (100)).toCSG()
+
+CSG sphere = new Sphere(50).toCSG()
+	.movez(40)
+
+CSG fullPiece = CSG.unionAll([mainCylinder, baseCylinder])
