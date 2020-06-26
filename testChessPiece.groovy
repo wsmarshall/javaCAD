193d8c8a8e668c4c6edd7baa11764fc65fff7ed9
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-26 15:16:24 -0700

    piece progress
    more pieces

--- testChessPiece.groovy
+++ testChessPiece.groovy
@@ -1,12 +1,14 @@
 //goal: black queen
 
-CSG topSphere = new Sphere(2).toCSG().movez(70)
+CSG topSphere = new Sphere(2).toCSG().movez(75)
+
+CSG topDome = new Cylinder (5, 0, 5, (100)).toCSG().movez(70)
 
 CSG mainCylinder  = new Cylinder(7.5, 5, 70, (100)).toCSG()
 
 CSG baseCylinder = new Cylinder(15, 6, 10, (100)).toCSG()
 
-CSG fullPiece = CSG.unionAll([mainCylinder, baseCylinder, topSphere])
+CSG fullPiece = CSG.unionAll([mainCylinder, baseCylinder,topDome, topSphere])
 
 fullPiece.setColor(javafx.scene.paint.Color.CYAN); //change this to black later
 
