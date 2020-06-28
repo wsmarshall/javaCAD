646575b28e907ec5778c2d67ee8df50188a31912
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-06-28 12:23:42 -0700

    progress
    work

--- testChessPiece.groovy
+++ testChessPiece.groovy
@@ -1,21 +1,26 @@
 //goal: black queen
 
-CSG topSphere = new Sphere(2).toCSG().movez(73)
+CSG topSphere = new Sphere(2).toCSG().movez(83)
 
-CSG topDome = new Cylinder (5, 0, 2, (100)).toCSG().movez(70)
+CSG topDome = new Cylinder (5, 0, 2, (100)).toCSG().movez(80)
 
 //need 2 polygons, 12 sided? dif of smaller from bigger for 'crown' piece
 CSG outerCrown = new Cylinder (5, 7, 3, (16)).toCSG()
 CSG innerCrown = new Cylinder (3.5, 5, 3, (16)).toCSG()
-CSG crown = outerCrown.difference(innerCrown).movez(67);
+CSG crown = outerCrown.difference(innerCrown).movez(77.5);
 
 
-CSG mainCylinder  = new Cylinder(7.5, 3, 70, (100)).toCSG()
+CSG mainCylinder  = new Cylinder(7.5, 3, 80, (100)).toCSG()
 
-CSG baseCylinder = new Cylinder(15, 6, 5, (100)).toCSG()
-CSG lowestTurn = 
+CSG baseCylinder = new Cylinder(16, 6, 5, (100)).toCSG()
+CSG lowestTurn = new RoundedCylinder(16, 3).cornerRadius(2).toCSG().movez(2.5)
+CSG lowCone = new Cylinder(15, 5, 10, (100)).toCSG().movez(5.5)
+CSG lowestRing = new RoundedCylinder(8, 4).cornerRadius(3).toCSG().movez(12)
 
-CSG fullPiece = CSG.unionAll([mainCylinder, baseCylinder,crown, topDome, topSphere])
+CSG topAssembly = CSG.unionAll([crown, topDome, topSphere])
+CSG bottomPiece = CSG.unionAll([baseCylinder, lowestTurn, lowCone, lowestRing, mainCylinder])
+
+CSG fullPiece = CSG.unionAll([bottomPiece, topAssembly])
 
 fullPiece.setColor(javafx.scene.paint.Color.CYAN); //change this to black later
 
