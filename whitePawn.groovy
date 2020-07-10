ba502bc2a42fe13b68dd813f3a4f456bb5a50d28
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-07-10 12:38:49 -0700

    attempt at parameterization
    for takeTwo branch

--- whitePawn.groovy
+++ whitePawn.groovy
@@ -1,1 +1,9 @@
-//Your code here
+//try at parameterizing an easier piece first
+CSG makepawn(){
+	//set up parameters to use
+	height = new LengthParameter("height of piece", 30, [120.0, 1.0])
+	width = new LengthParameter("width of piece", 10, [120.0, 1.0])
+	sphereSize = new LengthParameter("radius of top sphere", 8, [120.0, 1.0])
+}
+
+return makeQueen();
