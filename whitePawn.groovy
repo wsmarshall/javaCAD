babdaf1b39e9713075f912a7d36129a96b9ac89e
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-07-13 16:36:58 -0700

    tweak
    progress

--- whitePawn.groovy
+++ whitePawn.groovy
@@ -2,7 +2,7 @@
 CSG makepawn(){
 	//set up parameters to use
 	height = new LengthParameter("height of piece", 30, [120.0, 1.0])
-	width = new LengthParameter("width of piece", 10, [120.0, 1.0])
+	width = new LengthParameter("width of piece", 10, [100.0, 1.0])
 	sphereSize = new LengthParameter("radius of top sphere", 8, [120.0, 1.0])
 	squatFactor = new LengthParameter("how 'squished' the piece looks", 5, [110.0, 1.0])
 }
