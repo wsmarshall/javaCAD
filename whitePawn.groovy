a967bb5ae402ef9ddbf09c4d197c244d2e5b8675
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-07-22 16:37:29 -0700

    quick change to default sphereSize
    reflective of actual size requirements

--- whitePawn.groovy
+++ whitePawn.groovy
@@ -3,7 +3,7 @@ CSG makePawn(){
 	//set up parameters to use
 	height = new LengthParameter("height of piece", 30, [120.0, 1.0])
 	width = new LengthParameter("width of piece", 10, [100.0, 1.0])
-	sphereSize = new LengthParameter("radius of top sphere", 100, [120.0, 1.0])
+	sphereSize = new LengthParameter("radius of top sphere", 8, [120.0, 1.0])
 	squatFactor = new LengthParameter("how 'squished' the piece looks", 5, [110.0, 1.0])
 	//make the shapes that we need to combine
 	sphere = new Sphere(sphereSize).toCSG()
