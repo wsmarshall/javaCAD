1d26ce5efb0f31e77b79af414ac665f4a4a6c1ba
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-08-18 09:58:09 -0700

    parameter adjustment
    keeps piece recognizable even as internal proportions change

--- testChessPiece.groovy
+++ testChessPiece.groovy
@@ -3,40 +3,38 @@
 
 CSG makePiece(){
 //set up parameters to use
+	//default diameter of top sphere on head of piece
+	sphereSize = new LengthParameter("diameter of the topmost sphere", 2, [10, 1])
 	//default total height of the piece
 	height = new LengthParameter("height of piece", 60, [120.0, 1.0])
-	//default total diameter of the base of the piece
-	width = new LengthParameter("width of piece", 15, [120.0, 1.0])
-	//default diameter of top sphere on head of piece
-	sphereSize = new LengthParameter("diameter of the topmost sphere", 2, [10, 0.5])
-	//width of the connecting spine 
-	spineWidth = new LengthParameter("diameter of the connecting spine", 1, [1, 0.1])
+	//width of the connecting spine that runs the entire length(height) of the piece
+	coreWidth = new LengthParameter("diameter of the connecting spine", 1, [120, 1])
 //computation of constants from parameters
 	sideRes = (int) (30)//how rounded (faceted) the sides will be
 	double aSmidge = 0.5//half a millimeter 
-	double evenSmallestDistance = 0.0625 * width.getMM()//only a small ways, as compared to the defined width. defaults to 2
-	double smallestDistance = 0.2 * width.getMM() //slightly larger than a small way. defaults to 3
-	double smallerDistance = 0.3333 * width.getMM()//defaults to 5
-	double smallDistance = 0.5333 * width.getMM() //defaults to 8
-	double distance = 0.66667 * width.getMM() //defaults to 10
-	double bigDistance = width.getMM() //defaults to 15
-	double biggerDistance = 1.33333 * width.getMM() //defaults to 20
-	int largerWidth = 4 * width.getMM() //wider than the width parameter
-	double halfWidth = 0.5 * width.getMM() // half of the width parameter
+	double evenSmallestDistance = 2 * coreWidth.getMM()//only a small ways, as compared to the defined width. defaults to 2
+	double smallestDistance = 3 * coreWidth.getMM() //slightly larger than a small way. defaults to 3
+	double smallerDistance = 5 * coreWidth.getMM()//defaults to 5
+	double smallDistance = 8 * coreWidth.getMM() //defaults to 8
+	double distance = 10 * coreWidth.getMM() //defaults to 10
+	double bigDistance = 15 * coreWidth.getMM() //defaults to 15
+	double biggerDistance = 20 * coreWidth.getMM() //defaults to 20
+	int width = 32 * coreWidth.getMM() //original 'width' measurement, defaults to 32
+	int largerWidth = 64 * coreWidth.getMM() //wider than the width parameter, defaults to 64
 
 	double fourFifthsHeight = 0.8 * height.getMM()
 //make shapes to combine
 	//beginning of the top part, or the "head" of the piece
 		//the sphere at the very top
 	CSG topSphere = new Sphere(sphereSize).toCSG()	
-		.movez(height.getMM() - evenSmallestDistance) 
+		.movez(height.getMM()) 
 		//hemispheric dome that topSphere rests on
 	CSG topDome =  new Cylinder (smallerDistance, evenSmallestDistance, evenSmallestDistance, (sideRes)).toCSG()
 		.movez((height.getMM() - smallestDistance))
 		//outer part of the 'crown' piece
-	CSG outerCrown = new Cylinder(smallerDistance, smallestDistance + smallerDistance, smallestDistance, ((int) halfWidth)).toCSG()
+	CSG outerCrown = new Cylinder(smallerDistance, smallestDistance + smallerDistance, smallestDistance, ((int) smallDistance)).toCSG()
 		//inner part of the 'crown' piece
-	CSG innerCrown = new Cylinder(smallestDistance + aSmidge, smallerDistance, smallestDistance, ((int) halfWidth)).toCSG()
+	CSG innerCrown = new Cylinder(smallestDistance + aSmidge, smallerDistance, smallestDistance, ((int) smallDistance)).toCSG()
 		//difference of the outer and inner, gives "crown shell"
 	CSG crown = outerCrown.difference(innerCrown).movez(height.getMM() - smallerDistance);
 		//the cylinder that comes down from the crown and ends at the smallest ring
@@ -53,13 +51,13 @@ CSG makePiece(){
 		.movez((fourFifthsHeight - smallestDistance))
 	//beginning of the bottom part, or the base & body of the piece
 		//runs the length of the piece, allows user to access height as a UI parameter via slider
-	CSG connectingSpine = new Cylinder(spineWidth, spineWidth, height, (sideRes)).toCSG() 
+	CSG connectingSpine = new Cylinder(coreWidth, coreWidth, height, (sideRes)).toCSG() 
 		//the 'spine' of the whole piece
 	CSG mainCylinder  = new Cylinder(smallDistance, smallestDistance, fourFifthsHeight, (sideRes)).toCSG()
 		//very bottom cone of piece
-	CSG baseCylinder = new Cylinder(width, sphereSize, width, (sideRes)).toCSG()
+	CSG baseCylinder = new Cylinder(bigDistance, evenSmallestDistance, bigDistance, (sideRes)).toCSG()
 		//very bottom 'ring' - the 'crenelated' bezel just above the base
-	CSG lowestTurn = new RoundedCylinder(halfWidth, smallestDistance).cornerRadius(evenSmallestDistance).toCSG()
+	CSG lowestTurn = new RoundedCylinder(bigDistance, smallestDistance).cornerRadius(evenSmallestDistance).toCSG()
 		.movez(smallestDistance)
 		//'cone' piece that is visible/accessible to a hypothetical hand, near base (but not base)
 	CSG lowCone = new Cylinder(distance, smallerDistance, biggerDistance, (sideRes)).toCSG()
