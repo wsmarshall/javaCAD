c1bffa5758c577ea7205eddb8257b0cbeb3c1feb
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-08-19 10:01:02 -0700

    laid out comments
    setting up code structure

--- testKeychain.groovy
+++ testKeychain.groovy
@@ -1,1 +1,18 @@
-//Your code here
+//a combination key holder and bottle opener 
+
+CSG makeKeychain(){
+//set up parameters
+	//default length 
+
+	//default thickness (depth)
+
+	//default width
+
+//computation of constants from parameters
+
+//shape constructions for individual sections
+
+//assembly of the whole thing
+
+
+}
